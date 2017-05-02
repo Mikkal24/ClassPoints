7a9a66f7e0d033b8df51a6dda5df6789043183e7
Michael Sorensen <voltx180@gmail.com>
2017-05-01 19:43:13 -0700

    initial commit

---
+++ controllers/users_controller.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/User.js');
+
+router.get("/", function(req,res){
+
+});
+
+router.post("/", function(req,res){
+
+});
+
+router.put("/:id", function(req,res){
+
+});
+
+module.exports = router;
