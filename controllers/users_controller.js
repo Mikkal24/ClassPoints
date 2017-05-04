fe02aae1a1c7cae0d41c23b233175f357087b609
Michael Sorensen <voltx180@gmail.com>
2017-05-03 19:19:32 -0700

    added some routes to users_controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/User.js');
+var user = require('../models/User.js');
 
-router.get("/", function(req,res){
+router.get("/user", function(req,res){
+	//gets called when the main page loads probably
+	var students = []
+	user.findAll({}).then(function(data){
+		//how should we filter out the student that is already logged in?
+	})
+});
+
+router.get("/user/login", function(req,res){
+	//gets called when the user logs in (finds their user record essentially)
+	user.findOne({
+		where: {
+			//email is probably the most unique thing about the log in page?
+			email: req.params.email,
+		}
+	}).then(function(data){
+		var hbsObject = [data]
+
+		res.render("index",hbsObject)
+	})
+})
+
+router.post("/user", function(req,res){
 
 });
 
-router.post("/", function(req,res){
-
-});
-
-router.put("/:id", function(req,res){
+router.put("/user/:id", function(req,res){
 
 });
 
